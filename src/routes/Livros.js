94566407ab60bca623463fa3feaebb8a86fd6db3
Bruno Balen <brunobalen70@gmail.com>
2025-03-20 21:49:19 -0300

    validação

--- src/routes/Livros.js
+++ src/routes/Livros.js
@@ -1,11 +1,11 @@
 const controllers = require('../controllers/livros');
-
+const validacoes = require('../middlewares/validacaoDelete');
 //função que exporta as rotas e os métodos que
 //devem ser executados quando cada rota for chamada
 module.exports = (app) => {
     app.get('/livros', controllers.getLivros);
     app.get('/livros/:id([0-9]+)', controllers.getLivroPorId);
     app.post('/livros', controllers.postLivro);
-    app.delete('/livros/:id([0-9]+)', controllers.deleteLivro);
+    app.delete('/livros/:id([0-9]+)', validacoes.validarDelete , controllers.deleteLivro);
     app.patch('/livros/:id([0-9]+)', controllers.patchLivro);
 }
